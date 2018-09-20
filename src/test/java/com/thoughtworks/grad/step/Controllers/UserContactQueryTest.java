689e12c269ae328190d2a9022b073a9232981670
Lianqing/Bluto <lqqu@thoughtworks.com>
2018-09-20 23:12:51 +0800

    [xiaoqu][UserContact] user contact query exception.

--- src/test/java/com/thoughtworks/grad/step/Controllers/UserContactQueryTest.java
+++ src/test/java/com/thoughtworks/grad/step/Controllers/UserContactQueryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,6 +16,21 @@ public class UserContactQueryTest extends UserControllerTest {
         UserStorage.init(UserStorage.initUserDataWithContact());
         mockMvc.perform(get("/api/user/1/contacts"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$",hasSize(2)));
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    void should_get_bad_request_when_user_id_less_1() throws Exception {
+        UserStorage.init(UserStorage.initUserDataWithContact());
+        mockMvc.perform(get("/api/user/0/contacts"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void should_get_bad_request_with_error_msg_when_user_not_exists() throws Exception {
+        UserStorage.init(UserStorage.initUserDataWithContact());
+        mockMvc.perform(get("/api/user/12/contacts"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("user not exists"));
     }
 }
